98a97ef670e96e43b987de17d2471bf04014554e
Stephen S. Musoke <ssmusoke@gmail.com>
2018-06-25 12:13:33 +0300

    Initial Commit

---
+++ api/src/main/java/org/openmrs/module/ugandaemr/iss/UgandaEMRISSClinicActivator.java
@@ -0,0 +1,60 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.ugandaemr.iss;
+
+import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.Module;
+import org.openmrs.module.ModuleFactory;
+import org.openmrs.module.aijar.activator.HtmlFormsInitializer;
+import org.openmrs.module.aijar.activator.Initializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class contains the logic that is run every time this module is either started or shutdown
+ */
+public class UgandaEMRISSClinicActivator extends BaseModuleActivator {
+	
+	//private Log log = LogFactory.getLog(this.getClass());
+	
+	/**
+	 * @see #started()
+	 */
+	public void started() {
+		
+		try {
+			// run the initializers
+			for (Initializer initializer : getInitializers()) {
+				initializer.started();
+			}
+			// log.info("Started UgandaEMR ISS Clinic Module");
+		}
+		catch (Exception e) {
+			Module mod = ModuleFactory.getModuleById("ugandaemr-iss");
+			ModuleFactory.stopModule(mod);
+			throw new RuntimeException("failed to setup the module ", e);
+		}
+	}
+	
+	/**
+	 * @see #shutdown()
+	 */
+	public void shutdown() {
+		// log.info("Shutdown UgandaEMR ISS Clinic");
+	}
+	
+	private List<Initializer> getInitializers() {
+		List<Initializer> l = new ArrayList<Initializer>();
+		l.add(new HtmlFormsInitializer());
+		return l;
+	}
+	
+}
